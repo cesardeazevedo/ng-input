076d38f687886f44ef7dfa3567ab0f730f5eabc8
cesardeazevedo <cesardeazevedo@outlook.com>
2015-08-19 15:00:37 -0300

    Added ng-change to demo page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-angular.module('app', ['ngInput']);
+angular.module('app', ['ngInput'])
+.controller('AppController',function($scope){
+    $scope.change = function(){
+        console.log('Chagned 1');
+    };
+    $scope.change2 = function(){
+        console.log('Changed 2');
+    };
+    $scope.change3 = function(){
+        console.log('Changed 3');
+    };
+});
